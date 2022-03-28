e0190c92a4c7e7733e7f7bb73d7fc20e456cfdd2
Pranav Chary <pranavchary1993@gmail.com>
2022-03-27 19:39:51 -0500

    Start dicee and repo cloning

---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // IBOutlet: InterfaceBuilder Outlet
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        diceImageViewOne.image = UIImage(named: "DiceTwo");
+        diceImageViewOne.alpha = 0.5
+    }
+
+
+}
+
